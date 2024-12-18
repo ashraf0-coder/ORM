d797a9b410324c8a8f7d467e3461008c66487fd6
ashraf <sadarsign@gmail.com>
2024-12-18 21:39:37 +0300

    continues

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { defaultErrorMessage } from "../constants";
 const prisma = new PrismaClient()
 
 
 interface ICreateUserPayload {
-    username : String;
-    password : String;
+    username : string;
+    password : string;
+    phone_number: string;
 }
 export const createUser = async (req : Request, res : Response) => {
     try {
-        const { username, password} = req.body as ICreateUserPayload;
+        const { username, password, phone_number} = req.body as ICreateUserPayload;
 
-        if (!username || !password) {
+        if (!username || !password || !phone_number) {
             res.status(400).json({
             isSuccess: false,
             Message: "Validation error!"
@@ -23,20 +25,38 @@ export const createUser = async (req : Request, res : Response) => {
     const user = await prisma.users.create({
         data: {
             username: username,
-            password: password
+            password: password,
+            phone_number : phone_number
         }
     });
         res.status(200).json({
             isSucces: true,
-            message: "Succesfully fetched all users."
+            message: "Succesfully created new user."
         })
     } catch (error) {
         res.status(500).json({
             isSuccess : false,
-            message: "Something went wrong with the servet!"
+            message: "Something went wrong with the servet!",
+            error: JSON.stringify(error)
         })
         
     }
-}
+};
+
+export const getAllUsers = async (req : Request, res : Response) => {
+    try {
+        const users = await prisma.users.findMany();
+        res.status(200).json({
+            isSuccess: true,
+            users
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            isSucces : false,
+            message: defaultErrorMessage
+        });
+    }
+};
 
 
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json())
+
 app.use('/users', userRoute)
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
