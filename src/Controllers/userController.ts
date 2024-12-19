a330ceabf7a8842173d91bdcaf633816e0c69be7
ashraf <sadarsign@gmail.com>
2024-12-19 11:39:00 +0300

    added

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -9,6 +9,13 @@ interface ICreateUserPayload {
     password : string;
     phone_number: string;
 }
+
+interface IUpdateUser {
+    username : string,
+    phone_number : string,
+    password: string,
+    userId : number
+}
 export const createUser = async (req : Request, res : Response) => {
     try {
         const { username, password, phone_number} = req.body as ICreateUserPayload;
@@ -130,5 +137,59 @@ export const deleteUser = async (req : Request, res : Response) => {
             isSucces: false,
             message: defaultErrorMessage
         });   
+    }
+}
+
+
+
+export const updateUser = async (req : Request, res : Response) => {
+    try {
+        const { username, password, phone_number, userId } : IUpdateUser = req.body;
+        
+        if(!username || !password || !phone_number || !userId) {
+            res.status(400).json({
+                isSuccess: false,
+                message: "Validation error!",
+                payload: req.body
+            });
+            return;
+        }
+
+        const user = await prisma.users.findFirst({
+            where: {
+                id: userId
+            }
+        });
+
+        if(!user) {
+            res.status(404).json({
+                isSuccess: false,
+                message: "User not found!"
+            });
+            return;
+        }
+
+        const updatingUser = await prisma.users.update({
+            where: {
+                id: userId
+            },
+            data: {
+                username : username,
+                password : password,
+                phone_number : phone_number
+            }
+        });
+
+        res.status(200).json({
+            isSuccess: false,
+            user: updatingUser
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            isSucces: false,
+            message: defaultErrorMessage
+        });
     }
 }
--- src/Routes/userRoute.ts
+++ src/Routes/userRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createUser, deleteUser, getAllUsers, getOneUser } from "../Controllers/userController";
+import { createUser, deleteUser, getAllUsers, getOneUser, updateUser } from "../Controllers/userController";
 const router = Router();
 
 router.post('/create', createUser);
 router.get('/list', getAllUsers);
-router.get('/details/:userId', getOneUser)
-router.delete('/delete/:userId', deleteUser)
+router.get('/details/:userId', getOneUser);
+router.delete('/delete/:userId', deleteUser);
+router.put('/update', updateUser);
 
 
 export default router;
